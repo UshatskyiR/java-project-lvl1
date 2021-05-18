0847b1a7ba1ed3b9c0f0cb7f0f081f643503c410
UshatskyiR <r.ushatskyi@gmail.com>
2021-05-18 18:09:19 +0300

    update

---
+++ src/main/java/hexlet/code/Cli.java
@@ -0,0 +1,15 @@
+package hexlet.code;
+import java.util.Scanner;
+public class Cli {
+
+    public static String askName() {
+        Scanner nameAsk = new Scanner(System.in);
+        System.out.println("May I have your name?");
+        String name = nameAsk.nextLine();
+        return ("Hello, " + name + "!");
+
+    }
+
+}
+
+
